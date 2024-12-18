bf69afef00c46b3a71d39da7eac41c27902e424d
Ilya <zaxsad109@gmail.com>
2024-12-18 17:31:02 +0300

    downloaded lab-1

---
+++ src/lab-1/task-3/Implementations/MessageProcessor.cs
@@ -0,0 +1,48 @@
+using Itmo.Csharp.Microservices.Lab1.Messaging.Interfaces;
+using Itmo.Csharp.Microservices.Lab1.Messaging.Models;
+using Itmo.Dev.Platform.Common.Extensions;
+using System.Runtime.CompilerServices;
+using System.Threading.Channels;
+
+namespace Itmo.Csharp.Microservices.Lab1.Messaging.Implementations;
+
+public class MessageProcessor : IMessageSender, IMessageProcessor
+{
+    private readonly Channel<Message> _channel;
+    private readonly IMessageHandler _messageHandler;
+    private readonly int _batchSize;
+    private readonly TimeSpan _timeout;
+
+    public MessageProcessor(IMessageHandler messageHandler, int channelSize, int batchSize, TimeSpan timeout)
+    {
+        _messageHandler = messageHandler;
+        _batchSize = batchSize;
+        _timeout = timeout;
+        _channel = Channel.CreateBounded<Message>(new BoundedChannelOptions(channelSize)
+        {
+            FullMode = BoundedChannelFullMode.DropOldest,
+        });
+    }
+
+    public async ValueTask SendAsync(Message message, CancellationToken cancellationToken)
+    {
+        await _channel.Writer.WriteAsync(message, cancellationToken).ConfigureAwait(false);
+    }
+
+    public async Task ProcessAsync(CancellationToken cancellationToken)
+    {
+        ConfiguredCancelableAsyncEnumerable<IReadOnlyList<Message>> messagesChunked = _channel.Reader
+                                                                                               .ReadAllAsync(cancellationToken)
+                                                                                               .ChunkAsync(_batchSize, _timeout)
+                                                                                               .WithCancellation(cancellationToken);
+        await foreach (IReadOnlyList<Message> messages in messagesChunked)
+        {
+            await _messageHandler.HandleAsync(messages, cancellationToken).ConfigureAwait(false);
+        }
+    }
+
+    public void Complete()
+    {
+        _channel.Writer.Complete();
+    }
+}
---
+++ src/lab-1/task-3/Program.cs
@@ -0,0 +1,39 @@
+﻿using Itmo.Csharp.Microservices.Lab1.Messaging.Implementations;
+using Itmo.Csharp.Microservices.Lab1.Messaging.Interfaces;
+using Itmo.Csharp.Microservices.Lab1.Messaging.Models;
+
+internal class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var implementation = new MessageProcessor(new ConsoleHandler(), 100, 10, TimeSpan.FromSeconds(2));
+
+#pragma warning disable CA1859
+        IMessageProcessor processor = implementation;
+
+        IMessageSender sender = implementation;
+        var cts = new CancellationTokenSource();
+
+        Task processingTask = processor.ProcessAsync(cts.Token);
+
+        IEnumerable<Message> messages = GenerateMessages(100);
+
+        await Parallel.ForEachAsync(
+                messages,
+                cts.Token,
+                async (message, token) =>
+                {
+                    await sender.SendAsync(message, token).ConfigureAwait(false);
+                })
+            .ConfigureAwait(false);
+
+        processor.Complete();
+
+        await processingTask.ConfigureAwait(false);
+    }
+
+    private static IEnumerable<Message> GenerateMessages(int count)
+    {
+        return Enumerable.Range(0, count).Select(number => new Message($"Title {number}", $"Text {number}\n"));
+    }
+}
